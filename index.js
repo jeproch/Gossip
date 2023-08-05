1235c182566872fa3eb2d14722b85883c7cf26cb
jeproch <jprevenant@gmail.com>
2023-08-04 21:26:30 -0700

    Mobile unsupported, main page

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const inputName = document.getElementById("name-input");
+const inputPassword = document.getElementById("password-input");
 const submitBtn = document.getElementById("submit-btn");
+var nameVar;
+var passwordVar;
+var userAccess;
+
+document.addEventListener("DOMContentLoaded", function () {
+  var isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+
+  if (isMobile === true) {
+    window.location.href = "mobile.html";
+  }
+});
 
 submitBtn.addEventListener("click", function () {
-  const inputName = document.getElementById("name-input");
-  const inputPassword = document.getElementById("password-input");
+  nameVar = inputName.value;
+  passwordVar = inputPassword.value;
 
-  console.log(inputName.nodeValue + inputPassword.nodeValue);
-  console.log("works");
+  userAccess = nameVar + " " + passwordVar;
+  fileName = "home.html";
+  folderName = "home page";
+  window.location.href = folderName + "/" + fileName;
 });
